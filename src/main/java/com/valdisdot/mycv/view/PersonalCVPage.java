5b6b10d2cbc3041e38b7c838d646e46f4fd9dad4
valdis <valdisdot@gmail.com>
2024-05-27 15:15:03 +0300

    create implementation of CVPage for personal page

---
+++ src/main/java/com/valdisdot/mycv/view/PersonalCVPage.java
@@ -0,0 +1,22 @@
+package com.valdisdot.mycv.view;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.router.Route;
+import com.valdisdot.mycv.view.component.ImageGallery;
+import com.valdisdot.mycv.entity.page.ImageItemRecord;
+import com.valdisdot.mycv.storage.PageService;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Route(value = "/")
+public class PersonalCVPage extends CVPage {
+    public PersonalCVPage(PageService pageService) {
+        super(pageService.getPage());
+        makeGallery(pageService.getPage().getGallery()).ifPresent(this::addToLeftSide);
+    }
+
+    private Optional<Component> makeGallery(Collection<ImageItemRecord> imageItemRecords) {
+        return imageItemRecords == null || imageItemRecords.isEmpty() ? Optional.empty() : Optional.of(new ImageGallery(imageItemRecords.stream().map(ImageItemRecord::getPhoto).toList()));
+    }
+}
